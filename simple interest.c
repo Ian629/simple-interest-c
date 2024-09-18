a9e6f4cb12cf63d3232f352e983378891d2f38bb
Ian629 <mugambiiann@gmail.com>
2024-09-18 09:27:19 +0300

    Add files via upload
    perfect working

---
+++ simple interest.c
@@ -0,0 +1,18 @@
+// programto calculate simple interest
+#include <stdio.h> 
+int main () {
+  int principle_amount, time, rate ;
+    printf("Enter principle amount:");
+    scanf("%d" , &principle_amount); 
+    
+    printf("Eneter time:");
+    scanf("%d" ,&time); 
+    
+    printf("Enter rate:");
+    scanf("%d" , &rate);
+    
+     //printf(principle_amount*time*rate/100)\n;
+    
+    
+     return 0;
+}
